cb5cbd5ec2de9387eb81bf6863c0fe5b12be07cb
GitHub Action <action@github.com>
2022-06-23 04:30:17 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lilpoppy/elparametersupport/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lilpoppy.elparametersupport.services
+
+import com.github.lilpoppy.elparametersupport.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
